d3a5693dba5bba72f3db39ef44e470203a9b1aa3
aar-atiq <aar.msdeveloper01@gmail.com>
2021-01-23 01:42:40 +0600

    modified budgetCalculator

--- assignment.js
+++ assignment.js
@@ -9,51 +9,55 @@
 
 // end kilometerToMeter
 
-// function budgetCalculator(watch, phone, laptop) {
-//     var budge = watch * 50;
-//     var budge2 = phone * 100;
-//     var budge3 = laptop * 500;
-//     var totalBudget = budge + budge2 + budge3;
-
-//     return totalBudget;
-// }
-// var output = budgetCalculator(3, 3, 3);
-// console.log(output);
+function budgetCalculator(watch, phone, laptop) {
+    if (watch > 1 && phone > 1 && laptop > 1) {
+        var budge = watch * 50;
+        var budge2 = phone * 100;
+        var budge3 = laptop * 500;
+        var totalBudget = budge + budge2 + budge3;
+    }
+    else{
+        console.log("don't push negetive input");
+    }
+    return totalBudget;
+}
+var output = budgetCalculator(-1, -2, -3);
+console.log(output);
 
 // end budgetCalculator
 
 
-function hotelCost(howManyNight) {
-    var oneToTenNight = 10;
-    var elevenToTweentyNight = 20;
+// function hotelCost(howManyNight) {
+//     var oneToTenNight = 10;
+//     var elevenToTweentyNight = 20;
 
-    if (howManyNight > 1) {
+//     if (howManyNight > 1) {
 
-        if (howManyNight <= oneToTenNight) {
-            var cost1 = howManyNight * 100;
-            return cost1;
-        }
-        if ((howManyNight <= elevenToTweentyNight) && (howManyNight > oneToTenNight)) {
-            var cost2 = ((howManyNight - oneToTenNight) * 80);
-            var cost3 = (oneToTenNight * 100);
-            var cost4 = cost2 + cost3;
-            return cost4;
-        }
-        else {
-            if (howManyNight > elevenToTweentyNight) {
-                var cost6 = ((howManyNight - elevenToTweentyNight) * 50);
-                var cost8 = ((elevenToTweentyNight - oneToTenNight) * 80);
-                var cost3 = (oneToTenNight * 100);
-                var cost9 = cost3 + cost6 + cost8;
-                return cost9;
+//         if (howManyNight <= oneToTenNight) {
+//             var cost1 = howManyNight * 100;
+//             return cost1;
+//         }
+//         if ((howManyNight <= elevenToTweentyNight) && (howManyNight > oneToTenNight)) {
+//             var cost2 = ((howManyNight - oneToTenNight) * 80);
+//             var cost3 = (oneToTenNight * 100);
+//             var cost4 = cost2 + cost3;
+//             return cost4;
+//         }
+//         else {
+//             if (howManyNight > elevenToTweentyNight) {
+//                 var cost6 = ((howManyNight - elevenToTweentyNight) * 50);
+//                 var cost8 = ((elevenToTweentyNight - oneToTenNight) * 80);
+//                 var cost3 = (oneToTenNight * 100);
+//                 var cost9 = cost3 + cost6 + cost8;
+//                 return cost9;
 
-            }
-        }
+//             }
+//         }
 
-    }
-    else {
-        console.log("don't push negetive input")
-    }
-}
-var result = hotelCost(20);
-console.log(result);                             
+//     }
+//     else {
+//         console.log("don't push negetive input")
+//     }
+// }
+// var result = hotelCost(20);
+// console.log(result);                             
